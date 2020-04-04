4c31b2131f25b6332a0c196ffe2c1f85e4230f55
henry alvarez <alvarezhenry@hotmail.com>
2020-04-04 12:54:02 -0500

    Improve taxisController

---
+++ Taxi.Web/Data/Entities/TaxiEntity.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Taxi.Web.Data.Entities
+{
+    public class TaxiEntity
+    {
+        public int Id { get; set; }
+        [StringLength(6, MinimumLength = 6, ErrorMessage = " The {0} field must have {1} characters.")]
+        [RegularExpression(@"^([A-Za-z]{3}\d{3})$", ErrorMessage = "The field {0} must start with three characters and end witrh three numbers.")]
+        [Required(ErrorMessage = " the field {0} is mandatory.")]
+
+        public string Plaque { get; set; }
+
+    }
+}
